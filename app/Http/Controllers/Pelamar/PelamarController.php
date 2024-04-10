e157564745ee00aaec78bf0bcbd4a1cfa68896da
nuryamancode <iqbalnuryamann@gmail.com>
2024-04-10 21:10:52 +0700

    Terbaru

--- app/Http/Controllers/Pelamar/PelamarController.php
+++ app/Http/Controllers/Pelamar/PelamarController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class PelamarController extends Controller
 {
     public function index(){
-        return view('pelamar.dashboard');
+        return view('pelamar.lowongan');
     }
 }
