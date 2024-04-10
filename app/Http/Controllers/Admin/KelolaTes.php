17b229d0361a063834d32ddcdb9cd659ac17ec2a
nuryamancode <iqbalnuryamann@gmail.com>
2024-04-10 22:33:36 +0700

    Terbaru

--- app/Http/Controllers/Admin/KelolaTes.php
+++ app/Http/Controllers/Admin/KelolaTes.php
@@ -24,18 +24,18 @@ class KelolaTes extends Controller
 
     public function tambahTes(Request $request)
     {
-        $tesKemampuan = new TesKemampuanModel();
-        $tesKemampuan->keterangan = $request->keterangan;
-        $tesKemampuan->file_download = $request->file;
-        $tesKemampuan->lowongan_id = $request->judul;
-
         // Menyimpan
         if ($request->hasFile('file_download')) {
             $file = $request->file('file_download');
             $fileName = $file->getClientOriginalName();
-            $tesKemampuan->file_download->move('file_tes/', $fileName);
+            $file->move('file_tes/', $fileName);
+            $tesKemampuan = new TesKemampuanModel([
+                'file_download' => $fileName,
+                'keterangan' => $request->keterangan,
+                'lowongan_id' => $request->judul,
+            ]);
+            $tesKemampuan->save();
         }
-        $tesKemampuan->save();
         return redirect()->route('admin.kelolates.index')->with('success', 'Tes berhasil ditambahkan');
     }
 
